9531344f68a80c00cb8016fe012acca2cfb98f03
Maria Venkova <sniperhipo@gmail.com>
2025-05-29 17:17:45 +0300

    Add files via upload

--- RecipeListSO.cs
+++ RecipeListSO.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 //Blank space
 
-[CreateAssetMenu()]
+//[CreateAssetMenu()]
 public class RecipeListSO : ScriptableObject {
 
     public List<RecipeSO> recipeSOList;
