f4edd41565dcc8cbf2406e5aff759a9c668b5b87
Maria Venkova <sniperhipo@gmail.com>
2025-05-29 16:26:22 +0300

    Add files via upload

---
+++ DeliveryManager.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour {
+
+
+    private List<RecipeSO> waitingRecipeSOList;
+
+}
---
+++ RecipeListSO.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+//Blank space
+
+[CreateAssetMenu()]
+public class RecipeListSO : ScriptableObject {
+
+    public List<RecipeSO> recipeSOList;
+
+}
