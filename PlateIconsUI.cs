b3c5bc3cb567fdeb8b538e3576a4aff7e0710855
Maria Venkova <sniperhipo@gmail.com>
2025-05-30 16:46:55 +0300

    Add files via upload

--- PlateIconsUI.cs
+++ PlateIconsUI.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlateIconsUI : MonoBehaviour {
 
-
     [SerializeField] private PlateKitchenObject plateKitchenObject;
     [SerializeField] private Transform iconTemplate;
 
 
     private void Awake() {
-        iconTemplate.gameObject.SetActive(false);
+        iconTemplate.gameObject.SetActive(false); 
     }
 
     private void Start() {
