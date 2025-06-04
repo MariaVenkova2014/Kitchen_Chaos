93f6ff4d437bacb49d2ef7fbc7f07c79abf380b8
Maria Venkova <sniperhipo@gmail.com>
2025-06-04 17:18:50 +0300

    Add files via upload

--- DeliveryManagerSingleUI.cs
+++ DeliveryManagerSingleUI.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 //Blank space
 //Blank space
 
@@ -7,10 +8,27 @@ public class DeliveryManagerSingleUI : MonoBehaviour {
 
 
     [SerializeField] private TextMeshProUGUI recipeNameText;
+    [SerializeField] private Transform iconContainer;
+    [SerializeField] private Transform iconTemplate;
 
+
+    private void Awake() {
+        iconTemplate.gameObject.SetActive(false);
+    }
 
     public void SetRecipeSO(RecipeSO recipeSO) {
         recipeNameText.text = recipeSO.recipeName;
+
+        foreach (Transform child in iconContainer) {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in recipeSO.kitchenObjectSOList) {
+            Transform iconTransform = Instantiate(iconTemplate, iconContainer);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<Image>().sprite = kitchenObjectSO.sprite;
+        }
     }
 
 }
